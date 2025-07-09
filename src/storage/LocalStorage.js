c547b6a8333c58381b616ec5677ffa676ce7f4b9
Milind Yadav <milindcs07@gmail.com>
2025-07-09 13:59:36 +0530

    Delete src directory

--- src/storage/LocalStorage.js
+++
@@ -1,8 +0,0 @@
-export const getTransactions = () => {
-  const transactions = localStorage.getItem('transactions');
-  return transactions ? JSON.parse(transactions) : [];
-};
-
-export const setTransactions = (transactions) => {
-  localStorage.setItem('transactions', JSON.stringify(transactions));
-};
